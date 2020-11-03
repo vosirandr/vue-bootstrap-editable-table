963e3b7efefc2e4c61de032cc920b2681d56afa7
BiryukovNikita <wallement@gmail.com>
2020-11-03 08:41:03 +0700

    Fix integrating icons from bootstrap-vue

--- plugins/vue-plugins.js
+++ plugins/vue-plugins.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import { IconsPlugin } from 'bootstrap-vue';
 import VueJsonEditor from 'vue-json-editor';
 
+Vue.use(IconsPlugin);
 Vue.component('vue-json-editor', VueJsonEditor);
