2ec2e43276d6863713706a7a5d13aea20c1fe478
Nikita Biryukov <dwarfgrakula@gmail.com>
2021-03-31 10:36:56 +0700

    Rename JSON type

--- components/editable-table/column-types/JsonColumnType.js
+++ components/editable-table/column-types/JsonColumnType.js
@@ -3,7 +3,7 @@ import ColumnType from "./ColumnType";
 import tJsonCell from './t-json-cell';
 
 export default class JsonColumnType extends ColumnType {
-  static type = 'json';
+  static type = 'JSON';
   static cell = tJsonCell;
 
   static convertStringToValue(str) {
